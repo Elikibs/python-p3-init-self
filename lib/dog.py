c0e1d83a5eaf473398aa39ab8c277c7092b57a7d
Elisha <elishakibet67@gmail.com>
2023-12-06 08:42:28 +0300

    Update dog.py

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed= breed
+    
+max = Dog("Max")
+print(f"{max.name} is of breed {max .breed}")
+
+siba = Dog("Siba", "Bobel")
+print(f"{siba.name} is of breed {siba.breed}")
